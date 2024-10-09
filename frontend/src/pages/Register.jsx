5d72503956abc6a1242d73188e9ab688a40e3d85
elyesszobli <elyesszobli@gmail.com>
2024-10-09 22:13:01 +0200

    Ajouter la page register

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,78 @@
+
+function Register() {
+  return (
+    <section className="h-screen bg-sky-500">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <h1 className="flex items-center mb-6 text-4xl font-Oswald text-white">
+          ElyessLink
+        </h1>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h2 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Inscription
+            </h2>
+            <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Nom d utilisateur
+                    </label>
+                    <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="toto"
+                    required=""
+                    />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Adresse e-mail
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@gmail.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Mot de passe
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-sky-500 hover:bg-white hover:text-sky-500"
+              >
+                S inscrire
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Vous avez déja un compte ? 
+                <a
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline dark:text-sky-500 hover:text-white"
+                >
+                   se connecter
+                </a>
+              
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Register
