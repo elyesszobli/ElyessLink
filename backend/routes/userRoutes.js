cacd9a0fea6db431759a092947549bfd11276c75
elyesszobli <elyesszobli@gmail.com>
2024-11-11 13:38:04 +0100

    separer le front et le back

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Route();
-const userController = require("../controllers/userController");
-
-router.post('/login', userController);
-
-module.exports = router;
